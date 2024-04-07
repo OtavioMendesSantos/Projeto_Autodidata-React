28165f5def7a2b14ad1b066df506f421b905447c
OtavioMendesSantos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-04-07 13:45:51 -0300

    :black_nib:	course: Ferramentas de Automação

---
+++ React-Origamid/02-React_Para_Iniciantes/205-Ferramenta_Front-End/src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App(){
+    return <a href="https://otaviomendessantos.github.io/Sites_Portifolio/pages/formacao.html">Meu Portfólio</a>
+}
+
+export default App
