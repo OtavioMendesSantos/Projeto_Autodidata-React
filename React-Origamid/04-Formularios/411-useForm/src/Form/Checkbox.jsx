4b3d2394ff371ad28c38941801ec0f5eaf859db6
Otávio Mendes Santos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-05-27 09:47:04 -0300

    :black_nib:	course: useForm

---
+++ React-Origamid/04-Formularios/411-useForm/src/Form/Checkbox.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Checkbox = ({ value, setValue, options, ...props }) => {
+    const handleCheckbox = ({ target }) => {
+        if (target.checked) {
+            setValue([...value, target.value])
+        } else {
+            setValue(value.filter(itemValue => itemValue !== target.value))
+        }
+    }
+
+    return (
+        <p>
+            {options.map((option, index) => (
+                <label htmlFor={option} key={index}>
+                    <input
+                        style={{ display: 'inline-block', width: 'fit-content' }}
+                        type="checkbox"
+                        id={option}
+                        name={option}
+                        value={option}
+                        checked={value.includes(option)}
+                        onChange={handleCheckbox}
+                        {...props}
+                    />
+                    {option}
+                </label>
+            ))}
+        </p>
+    )
+}
+
+export default Checkbox
