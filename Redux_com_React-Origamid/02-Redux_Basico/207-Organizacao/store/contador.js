9c39305eabc5d8d7b0f2aab71fbc57ab35686222
Otávio Mendes Santos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-06-06 14:40:42 -0300

    :black_nib:	course: Organização

---
+++ Redux_com_React-Origamid/02-Redux_Basico/207-Organizacao/store/contador.js
@@ -0,0 +1,20 @@
+const INCREMENTAR = 'contador/INCREMENTAR';
+const DECREMENTAR = 'contador/DECREMENTAR';
+
+export const incrementar = () => ({ type: INCREMENTAR });
+export const decrementar = () => ({ type: DECREMENTAR });
+
+const initialState = 0;
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case INCREMENTAR:
+            return state + 1;
+        case DECREMENTAR:
+            return state - 1;
+        default:
+            return state;
+    }
+}
+
+export default reducer
