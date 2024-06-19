1b41773bc39128a389c75ffd95c19fd789d57cc4
Otávio Mendes Santos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-06-19 11:29:09 -0300

    :black_nib:	course: 0810 userContext

--- React-Origamid/08-Projeto_Final/src/App.jsx
+++ React-Origamid/08-Projeto_Final/src/App.jsx
@@ -5,20 +5,23 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Login from "./components/Login/Login";
+import { UserStorage } from "./UserContext";
 
 function App() {
   return (
     <div>
-      <BrowserRouter >
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
-    </div>
-  );
+    </div >
+  )
 }
 
 export default App;
--- React-Origamid/08-Projeto_Final/src/components/Header.jsx
+++ React-Origamid/08-Projeto_Final/src/components/Header.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
 import Dogs from '../Assets/dogs.svg?react';
+import { UserContext } from '../UserContext';
+
 const Header = () => {
+  const { data } = useContext(UserContext);
 
   return (
     <header className={styles.header}>
@@ -10,10 +13,17 @@ const Header = () => {
         <Link className={styles.logo} to="/" aria-label='Dogs - Home'>
           <Dogs />
         </Link>
-        <Link className={styles.login} to="/login">Login / Criar</Link>
+        {data
+          ? (
+            <Link className={styles.login} to="/conta">{data.nome}</Link>
+          ) : (
+            <Link className={styles.login} to="/login">Login / Criar</Link>
+          )
+        }
+
       </nav>
     </header>
-  );
+  )
 };
 
 export default Header;
