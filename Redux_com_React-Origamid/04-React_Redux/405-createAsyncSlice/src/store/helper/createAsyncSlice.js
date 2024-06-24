0ce1f09da124a7dc6e989ea22c6869bb3810d2fd
Otávio Mendes Santos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-06-24 13:05:17 -0300

    :black_nib:	course: 0405 createAsyncSlice

---
+++ Redux_com_React-Origamid/04-React_Redux/405-createAsyncSlice/src/store/helper/createAsyncSlice.js
@@ -0,0 +1,53 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+/**
+ * Cria um slice de uma função assíncrona
+ * @param {Object} config 
+ * @param {String} config.name
+ * @param {Object} config.initialState
+ * @param {Object} config.reducers
+ * @param {Function} config.fetchConfig
+ */
+
+const createAsyncSlice = (config) => {
+    const slice = createSlice({
+        name: config.name,
+        initialState: {
+            loading: false,
+            data: null,
+            error: null,
+            ...config.initialState
+        },
+        reducers: {
+            fetchStarted(state) {
+                state.loading = true
+            },
+            fetchSucess(state, action) {
+                state.loading = true
+                state.data = action.payload
+                state.error = false
+            },
+            fetchFailure(state, action) {
+                state.loading = true
+                state.data = null
+                state.error = action.payload
+            },
+            ...config.reducers
+        }
+    })
+    const { fetchStarted, fetchSucess, fetchFailure } = slice.actions
+    const asyncAction = (payload) => async (dispatch) => {
+        try {
+            dispatch(fetchStarted())
+            const { url, options } = config.fetchConfig(payload)
+            const response = await fetch(url, options)
+            const data = await response.json()
+            return dispatch(fetchSucess(data))
+        } catch (error) {
+            return dispatch(fetchFailure(error.message))
+        }
+    }
+    return { ...slice, asyncAction }
+}
+
+export default createAsyncSlice
