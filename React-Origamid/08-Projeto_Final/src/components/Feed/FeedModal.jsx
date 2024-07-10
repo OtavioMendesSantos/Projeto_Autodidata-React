7ac7646535f45a8f21a507cd56fce52d13996b0c
Otávio Mendes Santos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-07-10 12:18:56 -0300

    :black_nib:	course: 0821 Feed Modal 1

--- React-Origamid/08-Projeto_Final/src/components/Feed/FeedModal.jsx
+++ React-Origamid/08-Projeto_Final/src/components/Feed/FeedModal.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import styles from './FeedModal.module.css'
+import useFetch from '../../Hooks/useFetch'
+import { PHOTO_GET } from '../../api/Api'
+import Error from '../Helper/Error'
+import Loading from '../Helper/Loading'
+import PhotoContent from '../Photo/PhotoContent'
 
-const FeedModal = () => {
-  return (
-    <div>FeedModal</div>
-  )
+const FeedModal = ({ photo }) => {
+    const { data, loading, error, request } = useFetch()
+
+    useEffect(() => {
+        const { url, options } = PHOTO_GET(photo.id)
+        request(url, options)
+    }, [])
+
+    return (
+        <div className={styles.modal}>
+            {error && <Error>{error}</Error>}
+            {loading && <Loading />}
+            {data && <PhotoContent data={data} />}
+
+        </div>
+    )
 }
 
 export default FeedModal
