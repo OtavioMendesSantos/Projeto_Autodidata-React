bc8a719a39bcf432c3642e23759cd02ffc127688
OtavioMendesSantos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-07-14 19:21:09 -0300

    :black_nib:	course: 0833 Head

--- React-Origamid/08-Projeto_Final/src/components/Login/LoginCreate.jsx
+++ React-Origamid/08-Projeto_Final/src/components/Login/LoginCreate.jsx
@@ -1,46 +1,50 @@
-import React, { useContext } from 'react'
-import Input from '../Forms/Input'
-import Button from '../Forms/Button'
-import useForm from '../../Hooks/useForm'
-import { USER_POST } from '../../api/Api'
-import { UserContext } from '../../UserContext'
-import useFetch from '../../Hooks/useFetch'
-import Error from '../Helper/Error'
+import React, { useContext } from "react";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
+import { USER_POST } from "../../api/Api";
+import { UserContext } from "../../UserContext";
+import useFetch from "../../Hooks/useFetch";
+import Error from "../Helper/Error";
+import Head from "../Helper/Head";
 
 const LoginCreate = () => {
-  const username = useForm()
-  const email = useForm('email')
+  const username = useForm();
+  const email = useForm("email");
   // const password = useForm('password') // TODO: Depois eu mudo
-  const password = useForm()
-  const { userLogin } = useContext(UserContext)
-  const { loading, error, request } = useFetch()
+  const password = useForm();
+  const { userLogin } = useContext(UserContext);
+  const { loading, error, request } = useFetch();
 
   async function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
     const { url, options } = USER_POST({
       username: username.value,
       email: email.value,
-      password: password.value
-    })
-    const { response } = await request(url, options)
-    if (response.ok) userLogin(username.value, password.value)
+      password: password.value,
+    });
+    const { response } = await request(url, options);
+    if (response.ok) userLogin(username.value, password.value);
     console.log(response);
   }
 
   return (
-    <section className='animeLeft'>
-      <h1 className='title'>Cadastre-se</h1>
+    <section className="animeLeft">
+      <Head title="Crie Sua Conta" />
+      <h1 className="title">Cadastre-se</h1>
       <form onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="E-mail" type="text" name="email" {...email} />
         <Input label="Senha" type="text" name="password" {...password} />
-        {loading
-          ? <Button disabled>Cadastrando...</Button>
-          : <Button>Cadastrar</Button>}
+        {loading ? (
+          <Button disabled>Cadastrando...</Button>
+        ) : (
+          <Button>Cadastrar</Button>
+        )}
         <Error error={error} />
       </form>
     </section>
-  )
-}
+  );
+};
 
-export default LoginCreate
+export default LoginCreate;
--- React-Origamid/08-Projeto_Final/src/components/Login/LoginPasswordReset.jsx
+++ React-Origamid/08-Projeto_Final/src/components/Login/LoginPasswordReset.jsx
@@ -6,6 +6,7 @@ import useFetch from "../../Hooks/useFetch";
 import { PASSWORD_RESET } from "../../api/Api";
 import Error from "../Helper/Error";
 import { useNavigate } from "react-router-dom";
+import Head from "../Helper/Head";
 
 const LoginPasswordReset = () => {
   const navigate = useNavigate();
@@ -37,6 +38,7 @@ const LoginPasswordReset = () => {
 
   return (
     <section>
+      <Head title="Resete a Senha" />
       <h1 className="title">Resete a Senha</h1>
       <form onSubmit={handleSubmit}>
         <Input
