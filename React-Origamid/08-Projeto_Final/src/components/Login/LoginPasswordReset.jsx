08316a3de4e39eeb5fd316ccb95be7784bdce5d5
OtavioMendesSantos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-07-14 19:10:19 -0300

    :black_nib:	course: 0832 Perdeu Senha 2

--- React-Origamid/08-Projeto_Final/src/components/Login/LoginPasswordReset.jsx
+++ React-Origamid/08-Projeto_Final/src/components/Login/LoginPasswordReset.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
+import useFetch from "../../Hooks/useFetch";
+import { PASSWORD_RESET } from "../../api/Api";
+import Error from "../Helper/Error";
+import { useNavigate } from "react-router-dom";
 
 const LoginPasswordReset = () => {
-  return (
-    <div>LoginPasswordReset</div>
-  )
-}
+  const navigate = useNavigate();
+  const [login, setLogin] = useState("");
+  const [key, setKey] = useState("");
+  const password = useForm();
+  const { error, loading, request } = useFetch();
 
-export default LoginPasswordReset
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const k = params.get("key");
+    const l = params.get("login");
+    if (k) setKey(k);
+    if (l) setLogin(l);
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password.validate()) {
+      const { url, options } = PASSWORD_RESET({
+        login,
+        key,
+        password: password.value,
+      });
+      const { response, json } = await request(url, options);
+      if (response.ok) navigate("/login");
+    }
+  };
+
+  return (
+    <section>
+      <h1 className="title">Resete a Senha</h1>
+      <form onSubmit={handleSubmit}>
+        <Input
+          label="Nova Senha"
+          type="password"
+          name="password"
+          {...password}
+        />
+        {loading ? (
+          <Button disabled>Resetando...</Button>
+        ) : (
+          <Button>Resetar</Button>
+        )}
+      </form>
+      <Error error={error} />
+    </section>
+  );
+};
+
+export default LoginPasswordReset;
