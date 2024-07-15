adfb46cedff50ce9f5c784f974ca6059df409f8e
OtavioMendesSantos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-07-14 22:11:45 -0300

    :black_nib:	course: 0835 Biblioteca Externa 2

--- React-Origamid/08-Projeto_Final/src/components/User/UserStatsGraphs.jsx
+++ React-Origamid/08-Projeto_Final/src/components/User/UserStatsGraphs.jsx
@@ -1,12 +1,20 @@
 import React, { useEffect } from "react";
 import styles from "./UserStatsGraphs.module.css";
+import { VictoryPie, VictoryChart, VictoryBar } from "victory";
 
 const UserStatsGraphs = ({ data }) => {
   const [graph, setGraph] = React.useState([]);
   const [total, setTotal] = React.useState(0);
 
   useEffect(() => {
-    console.log(data);
+    const graphData = data.map((item) => {
+      return {
+        x: item.title,
+        y: Number(item.acessos),
+      };
+    });
+
+    setGraph(graphData);
     setTotal(
       data.map(({ acessos }) => Number(acessos)).reduce((a, b) => a + b, 0)
     );
@@ -14,8 +22,31 @@ const UserStatsGraphs = ({ data }) => {
 
   return (
     <section className={`animeLeft ${styles.graphs}`}>
-      <div className={`${styles.total}`}>
+      <div className={`${styles.total} ${styles.graphItem}`}>
         <p>Acessos: {total}</p>
+      </div>
+      <div className={`${styles.graphItem}`}>
+        <VictoryPie
+          data={graph}
+          innerRadius={50}
+          padding={{ top: 20, bottom: 20, left: 80, right: 80 }}
+          style={{
+            data: {
+                fillOpacity: .9,
+                stroke: "#fff",
+                strokeWidth: 2,
+            },
+            labels: {
+                fontSize: 14,
+                fill: "#333",
+            },
+          }}
+        />
+      </div>
+      <div className={`${styles.graphItem}`}>
+        <VictoryChart>
+          <VictoryBar alignment="start" data={graph} />
+        </VictoryChart>
       </div>
     </section>
   );
